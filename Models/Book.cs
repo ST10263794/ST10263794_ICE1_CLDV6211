a7e3a9f6cddc4637988567454c9e75b523d13b9c
ST10263794 <128512482+ST10263794@users.noreply.github.com>
2024-03-20 23:22:13 +0200

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LibraryApplication_ICE1_CLDV6211.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        public string Genre { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10263794_ICE1_CLDV6211.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
